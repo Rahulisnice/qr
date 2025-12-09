c044309ebda48e89eda7f3d18f3906c7ce606645
Rahul Yadav <rahul271435@gmail.com>
2025-12-09 11:41:02 +0530

    hi

--- qrcode-gen.js
+++ qrcode-gen.js
@@ -2,7 +2,7 @@
 const QRCode = require("qrcode");
 const fs = require("fs");
 
-const url = "https://yourdomain.com/scan.html"; // replace with your hosted page
+const url = "https://qr-va7d.onrender.com/scan.html"; // replace with your hosted page
 QRCode.toFile("qrcode.png", url, { errorCorrectionLevel: "H" }, function (err) {
   if (err) throw err;
   console.log("Saved qrcode.png for", url);
